41a8f10939e2e7bfc5736f344051edfc5e51b44c
evandalmeida <edalmei1@gmail.com>
2023-09-13 15:20:58 -0400

    completed

--- server/models.py
+++ server/models.py
@@ -9,4 +9,42 @@ metadata = MetaData(naming_convention={
 
 db = SQLAlchemy(metadata=metadata)
 
-# write your models here!
+
+class Restaurant(db.Model):
+    __tablename__ = "restaurants"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    location = db.Column(db.String)
+
+
+class Menus(db.Model):
+    __tablename__ = "menus"
+
+    id = db.Column(db.Integer, primary_key=True)
+    restraunt_id = db.relationship(db.Integer, db.ForeignKey("restraunts.id"))
+    
+    menu_item = db.Column(db.String)
+    price = db.Column(db.Float)
+
+
+
+
+    
+class Customer(db.Model):
+    __tablename__ = "customers"
+
+    id = db.Column(db.Integer, primary_key=True)
+    receipt_id = db.Column(db.Float, db.ForeignKey("reciepts.id"))
+   
+
+    total = db.relationship(db.Float, db.ForeignKey("menus.price"))
+    
+
+
+class Receipt(db.Model):
+    __tablename__ = "receipts"
+
+    id = db.Column(db.String, primary_key=True)
+
+    customer_id = db.Column(db.Integer, db.ForeignKey("customers.id"))
